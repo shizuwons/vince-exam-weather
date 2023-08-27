16c3643a5398ecb409696ddd6322bfa9806d5f77
Vince Dilig <dilig.vinz16@gmail.com>
2023-08-27 17:40:50 +0800

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="skyblue-bg">
+    <div class="input-field">
+      <input type="text" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+
+   }
+}
+</script>
